506a9377abb1c093f8fecb65e230a4f33977e3b6
Iceac Sarutobi <iceac88@gmail.com>
2016-02-27 01:14:21 +0100

    Refactoring of TeamMatcher
    Added language English
    change from Spinner to slider

--- de.icybits.util.TeamMatcher/src/de/icybits/util/TeamMatcher.java
+++ de.icybits.util.TeamMatcher/src/de/icybits/util/TeamMatcher.java
@@ -1,6 +1,8 @@
 package de.icybits.util;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -12,7 +14,11 @@ public class TeamMatcher extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws IOException {
-		VBox box = FXMLLoader.load(getClass().getResource("TeamMatcher.fxml"));
+		URL url = TeamMatcher.class.getResource("TeamMatcher.fxml");
+		ResourceBundle bundle = ResourceBundle.getBundle("i18n.TeamMatcher");
+		FXMLLoader loader = new FXMLLoader(url, bundle);
+		loader.setController(new TeamMatcherController());
+		VBox box = loader.load();
 		Scene scene = new Scene(box);
 		primaryStage.setScene(scene);
 		primaryStage.show();
--- de.icybits.util.TeamMatcher/src/de/icybits/util/TeamMatcherController.java
+++ de.icybits.util.TeamMatcher/src/de/icybits/util/TeamMatcherController.java
@@ -6,11 +6,11 @@ import java.util.List;
 
 import javafx.beans.Observable;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.SelectionMode;
-import javafx.scene.control.Spinner;
-import javafx.scene.control.SpinnerValueFactory.IntegerSpinnerValueFactory;
+import javafx.scene.control.Slider;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
@@ -29,7 +29,9 @@ public class TeamMatcherController {
 	@FXML
 	private TextArea textAreaResult;
 	@FXML
-	private Spinner<Integer> spinnerCount;
+	private Slider sliderCount;
+	@FXML
+	private Label labelCount;
 	@FXML
 	private ToggleGroup toggleGroupEvaluationType;
 	@FXML
@@ -37,28 +39,26 @@ public class TeamMatcherController {
 
 	@FXML
 	private void initialize() {
+		textFieldNewPlayer.setOnKeyReleased(e -> {
+			if (e.getCode() == KeyCode.ENTER) {
+				addPlayer();
+			}
+		});
+
 		listViewPlayers.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
 		listViewPlayers.setOnKeyReleased(e -> {
 			if (e.getCode() == KeyCode.BACK_SPACE || e.getCode() == KeyCode.DELETE) {
 				deletePlayers();
 			}
 		});
-		textFieldNewPlayer.setOnKeyReleased(e -> {
-			if (e.getCode() == KeyCode.ENTER) {
-				addPlayer();
-			}
-		});
-		IntegerSpinnerValueFactory valueFactory = new IntegerSpinnerValueFactory(1, 1, 0);
-		spinnerCount.setValueFactory(valueFactory);
-		listViewPlayers.getItems().addListener((Observable o) -> {
-			valueFactory.setMax(listViewPlayers.getItems().size());
-			textAreaResult.clear();
-		});
+		listViewPlayers.getItems().addListener((Observable o) -> sliderCount.setMax(listViewPlayers.getItems().size()));
+		labelCount.textProperty().bind(sliderCount.valueProperty().asString("%.0f"));
 	}
 
 	@FXML
 	private void addPlayer() {
-		listViewPlayers.getItems().add(textFieldNewPlayer.getText().trim());
+		if (isValidPlayerName(textFieldNewPlayer.getText().trim()))
+			listViewPlayers.getItems().add(textFieldNewPlayer.getText().trim());
 		textFieldNewPlayer.clear();
 	}
 
@@ -74,10 +74,21 @@ public class TeamMatcherController {
 		Collections.shuffle(playerList);
 		int teamSize;
 		if (toggleGroupEvaluationType.getSelectedToggle() == radioButtonByPlayerCount)
-			teamSize = spinnerCount.getValue();
+			teamSize = (int) sliderCount.getValue();
 		else
-			teamSize = spinnerCount.getValue() > 0 ? playerList.size() / spinnerCount.getValue() : 0;
-		textAreaResult.setText(buildTeams(playerList, teamSize));
+			teamSize = (int) (sliderCount.getValue() > 0 ? playerList.size() / sliderCount.getValue() : 0);
+		if (teamSize > 0)
+			textAreaResult.setText(buildTeams(playerList, teamSize));
+	}
+
+	private boolean isValidPlayerName(String string) {
+		if (string == null)
+			return false;
+		if (string.isEmpty())
+			return false;
+		if (listViewPlayers.getItems().contains(string))
+			return false;
+		return true;
 	}
 
 	private String buildTeams(List<String> playerList, int teamSize) {
